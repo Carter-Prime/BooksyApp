e3f9ca8df30c2c66ace1c59ac040b94f5a4fa333
Michael Carter <michael_carter_21@hotmail.com>
2021-02-06 11:24:49 +0200

    svg transformer installed and metro.config file added still working

--- views/Home.js
+++ views/Home.js
@@ -32,7 +32,7 @@ const Home = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
